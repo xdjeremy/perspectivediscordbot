c02d78b1ff96ece49dce16c9b019adbb1173882f
Jeremy Francisco <jeremyfrancisco@MacBook.local>
2021-04-11 15:39:08 +0800

    added perspective api. auto delete message

---
+++ main.js
@@ -0,0 +1,68 @@
+require('dotenv').config();
+const Discord = require('discord.js');
+const Perspective = require('perspective-api-client');
+const perspective = new Perspective({
+    apiKey: process.env.PERSPECTIVE_API
+});
+
+
+const treshhold = 0.75;
+const spamThreshhold = 0.50;
+
+async function evaluateMessage(message) {
+    try {
+        const result = await perspective.analyze({
+            "comment": {
+                "text": message
+            },
+            "requestedAttributes": {
+                "TOXICITY": {},
+                "INSULT": {},
+                "SPAM": {}
+            },
+            "doNotStore": true,
+        });
+        if (result.attributeScores.TOXICITY.summaryScore.value > treshhold ||
+            result.attributeScores.INSULT.summaryScore.value > treshhold ||
+            result.attributeScores.SPAM.summaryScore.value > spamThreshhold
+        ) {
+            return true;
+        }
+    } catch (err) {
+        console.log(err);
+        return false;
+    }
+}
+
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+
+client.on('message', async (message) => {
+    if (!message.guild || message.author.bot) return;
+
+    let detected = false;
+    try {
+        detected = await evaluateMessage(message.content);
+    } catch (err) {
+        console.log(err);
+    }
+
+    if (detected) {
+        message.delete();
+        message.reply('This is a christian server. Don\'t be stupid.')
+            .then(msg => {
+                msg.delete({
+                    timeout: 3000
+                });
+            });
+        return;
+    }
+
+
+})
+
+client.login(process.env.DISCORD_TOKEN);
